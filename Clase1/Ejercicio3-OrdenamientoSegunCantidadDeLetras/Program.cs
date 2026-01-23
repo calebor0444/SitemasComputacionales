db23eb2f9596022fdaeeb13ecb9b64f7432fb90c
calebor0444 <calebor04@gmail.com>
2026-01-23 15:11:09 -0600

    Ordenamiento por letras

---
+++ Clase1/Ejercicio3-OrdenamientoSegunCantidadDeLetras/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Ejercicio3_OrdenamientoSegunCantidadDeLetras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var laLista = new List<string>() { "Ana", "Juan", "Pedro", "Alejandro", "Maximiliano" };
+
+            laLista.Sort((nombre1, nombre2) =>
+            {
+                return nombre1.Length.CompareTo(nombre2.Length);
+            });
+
+            foreach (var item in laLista)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            //En caso de querer ordenar por orden de mas letras a menos o descendente invertimos las variables nombre1 y nombre 2 en la comparacion
+            //Ejemplo :
+            Console.WriteLine();
+            laLista.Sort((nombre1, nombre2) =>
+            {
+                return nombre2.Length.CompareTo(nombre1.Length);
+            });
+
+            Console.WriteLine("Mostrando ordenamiento por orden de más a menos");
+            foreach (var item in laLista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Clase1/Ejercicio3-OrdenamientoSegunCantidadDeLetras/readme.md
@@ -0,0 +1,12 @@
+# Ejercicio 3: Ordenamiento según cantidad de letras
+
+## Objetivo
+Trabajar con `List<string>` y aplicar operaciones básicas sobre colecciones.
+
+## Enunciado
+Dada una lista genérica que contiene varios nombres, ordenar los elementos de la lista de forma ascendente según la cantidad de caracteres que posee cada nombre.
+
+El nombre con menos letras debe aparecer primero y el nombre con más letras al final.
+
+## Restricción
+No se permite el uso de LINQ.
