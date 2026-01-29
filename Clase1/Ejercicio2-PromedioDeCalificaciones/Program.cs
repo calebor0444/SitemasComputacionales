2b63228a4a6eee4840699dd6533a916c2e524b77
calebor0444 <calebor04@gmail.com>
2026-01-28 19:26:10 -0600

    se actualiza

--- Clase1/Ejercicio2-PromedioDeCalificaciones/Program.cs
+++ Clase1/Ejercicio2-PromedioDeCalificaciones/Program.cs
@@ -10,7 +10,7 @@
             int laSuma = 0;
             double elPromedio = 0;
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 10; i++)
             {
                 var elValorRandom = elRandomizador.Next(100);
 
@@ -27,6 +27,14 @@
             Console.WriteLine($"La nota mayor: {laNotaMayor}");
             Console.WriteLine($"La nota menor: {laNotaMenor}");
             Console.WriteLine($"El promedio de las notas {elPromedio}");
+
+
+            Console.WriteLine("Valores de la lista");
+
+            foreach (var valor in lasCalificaciones)
+            {
+                Console.WriteLine(valor);
+            }
         }
     }
 }
